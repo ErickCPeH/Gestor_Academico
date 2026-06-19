406892112600995ecd588a61771f34d3d22cba77
Paolit-1 <paopaorojas3112@gmail.com>
2026-06-19 17:38:01 -0600

    Agregado de Materias Mock para interfaz de estudiantes

---
+++ src/app/data/materias.ts
@@ -0,0 +1,27 @@
+export interface Materia {
+  id_materia: number;
+  Nombre_Materia: string;
+}
+
+export const mockMateria: Materia[] = [
+  {
+    id_materia: 1,
+    Nombre_Materia: "Programación Orientada a Objetos",
+  },
+  {
+    id_materia: 2,
+    Nombre_Materia: "Bases de datos",
+  },
+  {
+    id_materia: 3,
+    Nombre_Materia: "Programación Avanzada",
+  },
+  {
+    id_materia: 4,
+    Nombre_Materia: "Sistemas Operativos",
+  },
+  {
+    id_materia: 5,
+    Nombre_Materia: "Redes",
+  },
+];
